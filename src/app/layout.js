f638c45ea6022e4273a36852daa60b2d4e37f8fb
gdhyani <gauravdhyani90@gmail.com>
2023-06-07 09:16:26 +0530

    main section

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,21 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Navbar from "./navbar";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Chakraa',
-  description: 'Marketing Agency',
-}
+    title: "Chakraa",
+    description: "Marketing Agency",
+};
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <Navbar />
+                {children}
+            </body>
+        </html>
+    );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,20 @@
 import Image from "next/image";
 import Link from "next/link";
-
+import "./page.css";
 export default function Home() {
     return (
-        <main className="flex h-screen align-middle justify-center flex-col items-center p-24">
-            <h1 className="font-extrabold text-7xl">Chakraa</h1>
-            <br></br>
-            <Link className="underline text-purple-400" href="https://www.instagram.com/chakraa360marketing" >Instagram</Link>
+        <main className=" h-screen overflow-hidden">
+            <section id="landing" className="px-5">
+                <div className="flex flex-col justify-center border h-screen">
+                    <h1 className="text-2xl relative top-10 px-5">We Are</h1>
+                    <div class="wrapper">
+                        <div class="marquee flex gap-2 ">
+                            <p>Social Media Marketing Agency. Social Media Marketing Agency.</p>
+                        </div>
+                    </div>
+                    <h1 className="text-xl w-1/2 px-5">With the right set of influencers based on their content style, demographics, audience and other metrics.</h1>
+                </div>
+            </section>
         </main>
     );
 }

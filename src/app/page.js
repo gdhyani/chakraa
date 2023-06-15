1f3477fa1a8a74188fe2eff8892157a82f9e4be7
gdhyani <gauravdhyani90@gmail.com>
2023-06-15 19:48:26 +0530

    section4

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,40 @@
 import Image from "next/image";
 import Link from "next/link";
 import "./page.css";
+import { useState } from "react";
+const section = [
+    {
+        name: "Whatsapp Marketing",
+        value: 1,
+        des: "WhatsApp marketing is a type of messenger marketing, which implies promoting your brand through WhatsApp. This channel helps your brand reach an enormous audience, build strong relationships with customers, and increase sales. At Socioclout we help you generate more buzz with broadcast messages and seeding CTAs in relevant WhatsApp groups.",
+    },
+    {
+        name: "Crisis Management",
+        value: 2,
+        des: "A crisis can occur as a result of an unpredictable event or an unforeseeable consequence of an event considered a potential risk. We help with crisis management by deploying strategies designed to help you deal with a sudden and significant negative event.",
+    },
+    {
+        name: "Influencer Campaigns",
+        value: 3,
+        des: "Influencer marketing is a type of social media marketing that uses endorsements and product mentions from influencers–individuals who have a dedicated social following and are viewed as experts within their niche. Our network of 100,000+ influencers means we are able to match the brand with the perfect influencers– no matter what the requirements or what the platform is.",
+    },
+    {
+        name: "Twitter Influencer",
+        value: 4,
+        des: "Twitter influencer marketing is partnering with Twitter accounts that engage in the brand’s area of interest, and collaborating with them to create brand-sponsored tweets. Tweets around viral and trending topics coupled with relevant hashtags from trusted influencers help garner higher engagement. Twitter significantly impacts the middle of the funnel and can help you drive conversations at scale. We help brands with Twitter Trending, Premium Twitter Influencers seeding and many more services.",
+    },
+    {
+        name: "Meme Marketing",
+        value: 5,
+        des: "Users consume huge quantities of memes on a daily basis; hence, memes make a great way by which marketers can catch and keep the audience’s attention. They have become the latest weapon for marketers because of their ability to communicate the story without the use of many words. We help the brands with the creation of memes in accordance with their campaigns. These memes enclose the brand’s message outlined with humour, keeping in mind our target audience so that they resonate well with the brand’s campaign.",
+    },
+];
 
 export default function Home() {
+    const [carosel, setCarosel] = useState(1);
+    function changeCaro(value) {
+        setCarosel(value);
+    }
     return (
         <main className="">
             <section id="landing" className=" px-3 sm:px-5">
@@ -157,9 +189,40 @@ export default function Home() {
             </section>
 
             {/* section4 */}
-            <section className="h-screen">
-                <div>
-                    
+            <section className=" flex flex-col mt-5 border py-5 mx-5 text-center">
+                {/* <div className=""> */}
+                <h1 className="font-bold text-5xl sm:text-7xl mt-10">
+                    What we Do
+                </h1>
+                {/* </div> */}
+                <div className="flex sm:flex-row flex-col gap-5 mt-20 sm:px-16 px-7 pb-10">
+                    <div className="sm:border-r-2 sm:border-b-0 border-b-2 border-red-600 sm:overflow-hidden overflow-x-scroll  sm:w-2/6 flex flex-row gap-2 sm:flex-col">
+                        {section.map((map) => (
+                            <button
+                                className={`p-3 ${
+                                    carosel == map.value
+                                        ? "bg-red-600"
+                                        : "bg-transparent"
+                                }`}
+                                onClick={() => {
+                                    changeCaro(map.value);
+                                }}
+                            >
+                                {map.name}
+                            </button>
+                        ))}
+                    </div>
+                    <div className="px-5 sm:w-4/6 mt-5 sm:mt-0">
+                        {section.map((map) => (
+                            <h1
+                                className={`${
+                                    carosel == map.value ? "block" : "hidden"
+                                } font-mono`}
+                            >
+                                {map.des}
+                            </h1>
+                        ))}
+                    </div>
                 </div>
             </section>
 
@@ -176,7 +239,14 @@ export default function Home() {
                     href="/contact"
                     className="flex flex-row gap-3 hover:scale-110 border-red-600  transition-all mt-12 sm:mt-0 bg-red-600 text-white rounded-xl px-5 py-4 text-2xl"
                 >
-                    Get in Touch <Image src="/arrow.svg" className="w-14" width={10} height={10} alt="arrow"/>
+                    Get in Touch{" "}
+                    <Image
+                        src="/arrow.svg"
+                        className="w-14"
+                        width={10}
+                        height={10}
+                        alt="arrow"
+                    />
                 </Link>
             </section>
         </main>
